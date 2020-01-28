60e906097c0d32e8f7ad193237d2ff0b1a97887f
Nolan Picini <nolanpic@gmail.com>
2020-01-28 12:13:53 -0800

    Create axiosWithAuth

---
+++ friends/utils/axiosWithAuth.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const axiosWithAuth = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    headers: { Authorization: localStorage.getItem('token') }
+});
+
+export default axiosWithAuth;
