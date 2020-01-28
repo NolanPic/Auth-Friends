38ca10f4551ba0b1b33c4cb5186a0d7d557f8fb4
Nolan Picini <nolanpic@gmail.com>
2020-01-28 12:58:59 -0800

    Add loading message to friends page

--- friends/src/pages/Friends.js
+++ friends/src/pages/Friends.js
@@ -15,25 +15,32 @@ const Friends = () => {
     }, []);
 
     return (
-        <table style={{ width: '100%' }}>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Age</th>
-                    <th>Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                {friends.map(friend => (
-                    <tr>
-                        <td>{friend.name}</td>
-                        <td>{friend.age}</td>
-                        <td>{friend.email}</td>
-                    </tr>
-                ))}
-            </tbody>
-            
-        </table>
+        <>
+            {friends.length ? (
+                <table style={{ width: '100%' }}>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {friends.map(friend => (
+                            <tr key={friend.id}>
+                                <td>{friend.name}</td>
+                                <td>{friend.age}</td>
+                                <td>{friend.email}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )
+            : (
+                <p>Loading...</p>
+            )}
+        </>
+        
     );
 };
 
