92a753b41e6f80cbc503bfa8cbc67796c94902b8
Nolan Picini <nolanpic@gmail.com>
2020-01-28 12:53:04 -0800

    Fix bug in axios auth

--- friends/src/utils/axiosWithAuth.js
+++ friends/src/utils/axiosWithAuth.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 export const baseURL = 'http://localhost:5000/api';
 
-const axiosWithAuth = axios.create({
-    baseURL,
-    headers: { Authorization: localStorage.getItem('token') }
-});
+const axiosWithAuth = () => {
+    return axios.create({
+        baseURL,
+        headers: { Authorization: localStorage.getItem('token') }
+    });
+};
 
 export default axiosWithAuth;
